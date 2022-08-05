08df400616ba5477712d3ac3eca8af933c6014ab
Leo <107925706+LeoYule0627@users.noreply.github.com>
2022-08-05 21:12:31 +0800

    Add files via upload

--- spring-practice/src/main/java/com/practice/springpractice/Controller/MealController.java
+++ spring-practice/src/main/java/com/practice/springpractice/Controller/MealController.java
@@ -1,25 +1,25 @@
 package com.practice.springpractice.Controller;
 
 import com.practice.springpractice.Model.Meal;
-import com.practice.springpractice.Model.Order;
 import com.practice.springpractice.Service.MealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/meal")
 public class MealController {
     @Autowired
-    MealService userService;
+    private MealService mealService;
 
     @GetMapping
-    public String getUserByName(@RequestParam String name, Model model){
-        Meal meal = this.userService.getMealByName(name);
-        model.addAttribute("meal",meal); //"meal"與html裡呼叫有關
-        return "Meal";  //"Meal"與html名稱有關
+    public String getMealByName(@RequestParam String name, Model model) {
+        Meal meal = this.mealService.getMealByName(name);
+        model.addAttribute("meal", meal); //"user"與html裡呼叫有關
+        return "meal";  //"Meal"與html名稱有關
     }
 
 }
--- spring-practice/src/main/java/com/practice/springpractice/Controller/OrderController.java
+++ spring-practice/src/main/java/com/practice/springpractice/Controller/OrderController.java
@@ -1,23 +1,35 @@
 package com.practice.springpractice.Controller;
 
 import com.practice.springpractice.Model.Order;
-import com.practice.springpractice.Service.MealService;
+import com.practice.springpractice.Service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 @Controller
 @RequestMapping(value = "/order")
 public class OrderController {
     @Autowired
-    MealService userService;
+    OrderService orderService;
 
     @GetMapping("/{seq}")
-    public String getUserById(@PathVariable int seq, Model model){
-        Order order = this.userService.getUserByOrder(seq);
+    public String getOrderBySeq(@PathVariable int seq, Model model){
+        Order order = this.orderService.getOrderBySeq(seq);
         model.addAttribute("order",order);
-        return "Order";
+        return "order";
+    }
+
+    @GetMapping("/income")
+    public String getIncome(Model model){
+        int income = this.orderService.getIncome();
+        model.addAttribute("income",income);
+        List<Order> orderList = this.orderService.getOrderList();
+        model.addAttribute("orderList",orderList);
+        return "income";
     }
 }
